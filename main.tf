fc62e2d14c04f4f22ed62eabff5052cf172b4100
Brendan Beck <brendan.beck62@gmail.com>
2022-08-13 19:54:31 -0700

    convert from helloworld

--- main.tf
+++ main.tf
@@ -20,6 +20,7 @@ provider "aws" {
 
 
 # S3 bucket to hold the zip payload
+# Creates the bucket
 resource "aws_s3_bucket" "lambda_bucket" {
   bucket = "${var.prefix}-lambda-bucket"
 
@@ -31,39 +32,40 @@ resource "aws_s3_bucket_acl" "lambda_bucket_acl" {
   acl    = "private"
 }
 
-data "archive_file" "lambda_hello_world" {
+data "archive_file" "lambda_payload" {
   type = "zip"
 
   source_dir  = "${path.module}/src"
-  output_path = "${path.module}/hello-world.zip"
+  output_path = "${path.module}/app.zip"
 }
 
-resource "aws_s3_object" "lambda_hello_world" {
+# uploads to the bucket
+resource "aws_s3_object" "lambda_bucket_payload" {
   bucket = aws_s3_bucket.lambda_bucket.id
 
-  key    = "hello-world.zip"
-  source = data.archive_file.lambda_hello_world.output_path
+  key    = "app.zip"
+  source = data.archive_file.lambda_payload.output_path
 
-  etag = filemd5(data.archive_file.lambda_hello_world.output_path)
+  etag = filemd5(data.archive_file.lambda_payload.output_path)
 }
 
 # lambda function
-resource "aws_lambda_function" "hello_world" {
-  function_name = "HelloWorld"
+resource "aws_lambda_function" "app" {
+  function_name = var.prefix
 
   s3_bucket = aws_s3_bucket.lambda_bucket.id
-  s3_key    = aws_s3_object.lambda_hello_world.key
+  s3_key    = aws_s3_object.lambda_bucket_payload.key
 
   runtime = "nodejs16.x"
-  handler = "hello.handler"
+  handler = "${var.prefix}.handler"
 
-  source_code_hash = data.archive_file.lambda_hello_world.output_base64sha256
+  source_code_hash = data.archive_file.lambda_payload.output_base64sha256
 
   role = aws_iam_role.lambda_exec.arn
 }
 
-resource "aws_cloudwatch_log_group" "hello_world" {
-  name = "/aws/lambda/${aws_lambda_function.hello_world.function_name}"
+resource "aws_cloudwatch_log_group" "lambda-logs" {
+  name = "/aws/lambda/${aws_lambda_function.app.function_name}"
 
   retention_in_days = 30
 }
